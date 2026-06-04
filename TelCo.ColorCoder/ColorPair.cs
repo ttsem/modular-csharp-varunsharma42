633b460996a30b1cac993a5d86040e9a40cb56b7
Varun S <90321164+varunsharma42@users.noreply.github.com>
2026-06-04 09:00:01 +0530

    Update ColorPair.cs

--- TelCo.ColorCoder/ColorPair.cs
+++ TelCo.ColorCoder/ColorPair.cs
@@ -1,1 +1,13 @@
+using System.Drawing;
 
+namespace TelCo.ColorCoder
+{
+    internal class ColorPair
+    {
+        public Color majorColor;
+        public Color minorColor;
+
+        public override string ToString() => 
+            string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
+    }
+}
