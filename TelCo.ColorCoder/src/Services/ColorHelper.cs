dfea7f14890d495cef824847c6635a8523b01aae
Varun S <90321164+varunsharma42@users.noreply.github.com>
2026-06-04 09:32:49 +0530

    Update ColorHelper.cs

--- TelCo.ColorCoder/src/Services/ColorHelper.cs
+++ TelCo.ColorCoder/src/Services/ColorHelper.cs
@@ -6,39 +6,52 @@ namespace TelCo.ColorCoder.Services
 {
     public static class ColorHelper
     {
-        private static readonly Color[] colorMapMajor = { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
-        private static readonly Color[] colorMapMinor = { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
-        
+        private static readonly Color[] _majorColors =
+        {
+            Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet
+        };
+
+        private static readonly Color[] _minorColors =
+        {
+            Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray
+        };
+
         /// <summary>
-        /// Given a pair number function returns the major and minor colors in that order
+        /// Given a pair number, returns the major and minor colors in that order.
         /// </summary>
         /// <param name="pairNumber">Pair number of the color to be fetched</param>
-        /// <returns></returns>
+        /// <returns>ColorPair</returns>
         public static ColorPair GetColorFromPairNumber(int pairNumber)
         {
             if (pairNumber < 1 || pairNumber > 25)
                 throw new ArgumentOutOfRangeException(nameof(pairNumber), "Outside allowed range");
 
             int zeroBased = pairNumber - 1;
-            return new ColorPair { 
-                MajorColor = MajorColors[zeroBased / MinorColors.Length], 
-                MinorColor = MinorColors[zeroBased % MinorColors.Length] 
+
+            return new ColorPair
+            {
+                MajorColor = _majorColors[zeroBased / _minorColors.Length],
+                MinorColor = _minorColors[zeroBased % _minorColors.Length]
             };
         }
+
         /// <summary>
-        /// Given the two colors the function returns the pair number corresponding to them
+        /// Given the two colors, returns the pair number corresponding to them.
         /// </summary>
         /// <param name="pair">Color pair with major and minor color</param>
-        /// <returns></returns>
-       public static int GetPairNumberFromColor(ColorPair pair)
+        /// <returns>Pair number</returns>
+        public static int GetPairNumberFromColor(ColorPair pair)
         {
-            int majorIndex = Array.IndexOf(MajorColors, pair.MajorColor);
-            int minorIndex = Array.IndexOf(MinorColors, pair.MinorColor);
+            if (pair == null)
+                throw new ArgumentNullException(nameof(pair));
+
+            int majorIndex = Array.IndexOf(_majorColors, pair.MajorColor);
+            int minorIndex = Array.IndexOf(_minorColors, pair.MinorColor);
 
             if (majorIndex == -1 || minorIndex == -1)
-                throw new ArgumentException("Unknown Colors");
+                throw new ArgumentException("Unknown colors");
 
-            return (majorIndex * MinorColors.Length) + (minorIndex + 1);
+            return (majorIndex * _minorColors.Length) + (minorIndex + 1);
         }
     }
 }
